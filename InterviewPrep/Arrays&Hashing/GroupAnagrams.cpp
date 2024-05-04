0e749509096742ad5a433ae6fa7eab58239a68bd
dubey22rohit <dubey22rohit@gmail.com>
2024-05-04 23:54:15 +0530

    added new problem/solutions

--- InterviewPrep/Arrays&Hashing/GroupAnagrams.cpp
+++ InterviewPrep/Arrays&Hashing/GroupAnagrams.cpp
@@ -33,8 +33,39 @@ vector<vector<string>> groupAnagrams(vector<string>& strs) {
   return ans;
 }
 
+vector<vector<string>> groupAnagramsOp(vector<string>& strs) {
+  unordered_map<string, int> hm;
+  vector<vector<string>> ans;
+  for (int i = 0; i < strs.size(); ++i) {
+    string tmp = strs[i];
+    sort(tmp.begin(), tmp.end());
+    if (hm.find(tmp) != hm.end()) {
+      ans[hm[tmp]].push_back(strs[i]);
+    } else {
+      hm[tmp] = ans.size();
+      ans.push_back({strs[i]});
+    }
+  }
+  return ans;
+}
 int main() {
   vector<string> strs{"eat", "tea", "tan", "ate", "nat", "bat"};
+  vector<vector<string>> ans = groupAnagrams(strs);
+  vector<vector<string>> ansOp = groupAnagramsOp(strs);
+  for (auto sarr : ans) {
+    for (auto s : sarr) {
+      cout << s << ", ";
+    }
+    cout << "\n";
+  }
 
+  cout << "\nOp:\n";
+
+  for (auto sarr : ansOp) {
+    for (auto s : sarr) {
+      cout << s << ", ";
+    }
+    cout << "\n";
+  }
   return 0;
 }
